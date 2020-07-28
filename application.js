5562007fbdc4fc6857c8c6043751875584480212
Jacob Arendt <arendt404@gmail.com>
2020-07-27 21:19:52 -0500

    Import React-Router

--- application.js
+++ application.js
@@ -1,5 +1,5 @@
+import { Router, Route, Link } from "react-router-dom";
 var PropTypes = 'prop-types',
-{Router, Route, IndexRoute, Link, browserHistory} = ReactRouter,
 darkBackground = "linear-gradient(#01081C, #002AA0)",
 lightBackground = "linear-gradient(#004BAE, #99D9FC)",
 hours = new Date().getHours(),
