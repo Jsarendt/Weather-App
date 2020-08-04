6d571229b7a15d3c531eaef37b2f3c58cba366d7
Jacob Arendt <arendt404@gmail.com>
2020-08-03 20:03:37 -0500

    webpack test

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
   mode: 'development',  
   entry: './src/index.js',
   output: {
-    filename: 'bundle.[hash].js'
+	path: path.resolve(__dirname, 'temp'),
+    filename: 'bundle.js'
   },
   devtool: 'inline-source-map',
   module: {
