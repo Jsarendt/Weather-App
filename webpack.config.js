1b5320824d73537af4cf41eb1d30793c1f038a1f
Jacob Arendt <arendt404@gmail.com>
2020-08-05 19:43:34 -0500

    test

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   mode: 'development',  
   entry: './application.js',
   output: {
-	path: path.resolve(__dirname, './dist'),
+	path: path.join(__dirname, 'dist'),
 	publicPath: '/',
     filename: 'bundle.js'
   },
