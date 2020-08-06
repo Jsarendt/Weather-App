924cfe3d3bc69f8499c1b4c617f443b26518829e
Jacob Arendt <arendt404@gmail.com>
2020-08-05 20:05:27 -0500

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 module.exports = {
   mode: 'development',  
