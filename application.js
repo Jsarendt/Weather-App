bd7440ced4110373e44befc25d1a2f0c15ba93cb
Jacob Arendt <arendt404@gmail.com>
2020-08-09 17:28:43 -0500

    test

--- application.js
+++ application.js
@@ -1,7 +1,6 @@
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import routes from './routes.js';
-import PropTypes from 'prop-types';
-
+import DelayLink from './DelayLink.js.txt';
 var Link = require('react-router').Link;
 var createReactClass = require('create-react-class');
 var React = require('react');
@@ -21,61 +20,6 @@ days = [
 	{id: "Thursday", abb: "Thurs"}, 
 	{id: "Friday", abb: "Fri"}, 
 	{id: "Saturday", abb: "Sat"}];
-
-//delays rendering of /application route to allow for the Geocoding API call to make changes
-export class DelayLink extends React.Component {
-  static propTypes = {
-    delay:        PropTypes.number,
-    onDelayStart: PropTypes.func,
-    onDelayEnd:   PropTypes.func
-  };
-  static defaultProps = {
-    delay:        0,
-    onDelayStart: () => {},
-    onDelayEnd:   () => {}
-  };
-  static contextTypes = Link.contextTypes;
-  constructor(props) {
-    super(props);
-    this.timeout = null;
-  };
-  componentWillUnmount() {
-    if (this.timeout) {
-      clearTimeout(this.timeout);
-    }
-  };
-
-  handleClick = (e) => {
-    const { replace, to, delay, onDelayStart, onDelayEnd } = this.props;
-    const history = this.context.router;
-		
-    onDelayStart(e, to);
-    if (e.defaultPrevented) {
-      return;
-    }
-		
-    e.preventDefault();
-		
-    this.timeout = setTimeout(() => {
-      if (replace) {
-        history.replace(to);
-      } else {
-        history.push(to);
-      }
-      onDelayEnd(e, to);
-    }, delay);
-  };
-
-  render() {
-    const props = Object.assign({}, this.props);
-    delete props.delay;
-    delete props.onDelayStart;
-    delete props.onDelayEnd;
-    return (
-      <Link {...props} onClick={this.handleClick} >Search</Link>
-    );
-  }
-}
 
 
 export class CitySearch extends React.Component {
