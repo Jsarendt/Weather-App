d937d2364383c9f13377c09cbb8208e6ea867094
Jacob Arendt <arendt404@gmail.com>
2020-08-09 19:28:06 -0500

    test

--- routes.js
+++ routes.js
@@ -3,10 +3,10 @@ import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import { CitySearch, Application, WeekCard } from './application.js';
 
 const createRoutes = (
-	<Router basename="/weather-app">
-		<Route exact path='/' component={CitySearch} />
-		<Route path='/application' component={Application}>
-			<Route path='/week' component={WeekCard} />
+	<Router>
+		<Route exact path={process.env.PUBLIC_URL + '/'} component={CitySearch} />
+		<Route path={process.env.PUBLIC_URL + '/application'} component={Application}>
+			<Route path={process.env.PUBLIC_URL + '/week'} component={WeekCard} />
 		</Route>
 	</Router>
 )
